7de92e5d5537974fd3c7ad70075db8dcfd2edf10
TejeshwarRai <tejeshwarrai@gmail.com>
2019-06-28 11:59:16 +0530

    Pandas

---
+++ D28.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+"""
+num = [11,22, 33, 44, 55]
+s1 = pd.Series(num)
+print(s1)
+
+age = {"john":20,"jim":30,"jack":40,"joe":50}
+s2 = pd.Series(age)
+print(s2)
+
+print(s1[0])
+print(s2["jim"])
+
+print(s1[1:])
+print(s1[:3])
+print(s1[2:5])
+
+print(s2["jim":])
+print(s2["jim":"joe"])
+"""
+#
+# num1 = [10,20, 30, 40, 50]
+# num2 = [11,22, 33, 44, 55]
+#
+# emp1 = {"eid":101, "name":"John", "age":20}
+# emp2 = {"eid":102, "name":"Fionna", "age":22}
+# emp3 = {"eid":103, "name":"Kia", "age":24}
+#
+# df1 = pd.DataFrame([num1, num2])
+# df2 = pd.DataFrame([emp1, emp2, emp3])
+#
+# print(df1)
+# print()
+# print(df2)
+# print()
+#
+# print(df1[0])
+# print()
+# print(df2["eid"])
+#
+# print()
+# print(df2["eid"][0])
+# print(df2[:][:])
+
+# bla = pd.read_csv("file.csv")
+"""
+y = [10,20,30,40,50]
+plt.plot(y)
+plt.show()
+"""
+
+x = list(range(0,11))
+y1 = [n for n in x]
+y2 = [n*n for n in x]
+y3 = [n*n*n for n in x]
+
+print(x)
+print(y1)
+print(y2)
+print(y3)
+
+plt.plot(x,y1, label = "y1")
+plt.plot(x,y2, label = "y2")
+plt.plot(x,y3, label = "y3")
+plt.legend()
+
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+
+plt.title("Polynomial Graphs")
+plt.grid("true")
+plt.grid(True)
+
+plt.xlim(0, 50)
+plt.ylim(0, 2000)
+
+plt.savefig("1st Graph")        # to save
+plt.show()
