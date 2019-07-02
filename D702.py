4ba968503c15e6e11eedfaddbdde7d0a162b6462
TejeshwarRai <tejeshwarrai@gmail.com>
2019-07-02 12:31:10 +0530

    pandas

---
+++ D702.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+# arr = np.arange(1, 51)
+# arr = list(range(1,51))
+# s1 = pd.Series(arr)
+# print(s1)
+# print(s1.axes)
+# print(s1.values)
+# print(type(s1.values))
+# print(s1.head(5))
+# print(s1.tail(5))
+
+# od = np.arange(1, 100, 2)
+# eve = np.arange(0, 100, 2)
+#
+# data = {"odd": od, "even": eve}
+# frame = pd.DataFrame(data)
+# print(frame.sum)
+# print(frame.max)
+# print(frame.min)
+"""
+a = input("Enter 1st number")
+b = input("Enter 2nd number")
+op = input("Enter operator")
+ope = a + " " + op + " " + b
+print(ope, end=" = ")
+print(eval(ope))
+"""
+# sql join
